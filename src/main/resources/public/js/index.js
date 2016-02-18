30d733aba8bbae688a65a3c16beaf476977301c5
TianchengDong <DavidTung@David-MacBookPro.local>
2016-02-17 22:27:56 -0500

    <Feb17-4>

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,39 +1,4 @@
-/* Ripple-effect animation */
-(function($) {
-    $(".ripple-effect").click(function(e){
-        var rippler = $(this);
-
-        // create .ink element if it doesn't exist
-        if(rippler.find(".ink").length == 0) {
-            rippler.append("<span class='ink'></span>");
-        }
-
-        var ink = rippler.find(".ink");
-
-        // prevent quick double clicks
-        ink.removeClass("animate");
-
-        // set .ink diametr
-        if(!ink.height() && !ink.width())
-        {
-            var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
-            ink.css({height: d, width: d});
-        }
-
-        // get click coordinates
-        var x = e.pageX - rippler.offset().left - ink.width()/2;
-        var y = e.pageY - rippler.offset().top - ink.height()/2;
-
-        // set .ink position and add class .animate
-        ink.css({
-          top: y+'px',
-          left:x+'px'
-        }).addClass("animate");
-    })
-})(jQuery);
-
-
-/* vavbar animation */
+/* navbar animation */
 $("#toggle").click( function(event){
     event.preventDefault();
     if ($(this).hasClass("isDown") ) {
@@ -83,10 +48,5 @@ $('.responsive').on('click', '.close', function(){
 
 /* favorite animation */
 $('.favorite').click(function(){
-  $(this).toggleClass('favorited');
+    $(this).toggleClass('favorited');
 });
-
-/* tooltip animation */
-$(document).ready(function(){
-		    $('[data-toggle="tooltip"]').tooltip();
-		});
