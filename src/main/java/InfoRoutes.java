c55a7189dee13712c0ce57e85f34666f86e50a91
TianchengDong <tid17@pitt.edu>
2016-03-25 14:31:24 -0400

    <Mar25>

--- src/main/java/InfoRoutes.java
+++ src/main/java/InfoRoutes.java
@@ -19,10 +19,24 @@ public class InfoRoutes {
     private void setupRoutes() {
       get("/api/about", (req, res) -> {
           Map<String, Object> data = new HashMap<>();
-          data.put("city", "Beijing");
+          data.put("city1", "Beijing");
+          data.put("city2", "Shanghai");
+          data.put("city3", "Xi'an");
+          data.put("city4", "Chengdu");
+          data.put("city5", "Hongkong");
+          data.put("city6", "Taipei");
           data.put("description", "Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.");
           return data;
       }, gson::toJson);
+
+
+      get("/api/name", (req, res) -> {
+          Map<String, Object> data = new HashMap<>();
+          data.put("name", "David");
+          return data;
+      }, gson::toJson);
+
+
 
         // get("/api/time/now", (req, res) -> {
         //     Map<String, Object> data = new HashMap<>();
